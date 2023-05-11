459f124b3c1f83714ecd5b010e09a1f36e26a0b0
Duotduot <13892@coderacademy.edu.au>
2023-05-11 13:38:11 +1000

    Delete password funtion completed

--- main.py
+++ main.py
@@ -67,5 +67,18 @@ def delete_password():
     if not os.path.isfile(PASSWORD_FILE):
         print("No passwords saved yet.")
         return 
+    
+    site = input("Enter the site name: ")
+
+    with open(PASSWORD_FILE, 'r') as f:
+        passwords = json.load(f)
+
+    if site in passwords:
+        del passwords[site]
+        with open(PASSWORD_FILE, 'w') as f:
+            json.dump(passwords, f)
+        print(f"{site}'s password deleted successfully.")
+    else:
+        print(f"No password found for {site}.")
 
 main()
